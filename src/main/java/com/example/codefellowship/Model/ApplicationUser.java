a86228ef48072a32aafba9c8f417f71f99645fcf
obada-naser <obadanaser135@gmail.com>
2021-11-01 01:05:37 +0200

    security and othe methods

---
+++ src/main/java/com/example/codefellowship/Model/ApplicationUser.java
@@ -0,0 +1,73 @@
+package com.example.codefellowship.Model;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class ApplicationUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY  )
+    private int Id;
+    @Column(unique = true)
+    private String userName;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private int dateOfBirth;
+    private String bio;
+
+    public ApplicationUser(){
+
+    }
+
+    public ApplicationUser(String userName, String password, String firstName, String lastName, int dateOfBirth, String bio) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.bio = bio;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/codefellowship/Repositories/AppUserRepository.java
@@ -0,0 +1,9 @@
+package com.example.codefellowship.Repositories;
+
+import com.example.codefellowship.Model.ApplicationUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppUserRepository extends CrudRepository<ApplicationUser,Integer> {
+    ApplicationUser findByUserName(String userName);
+
+}
