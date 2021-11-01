263af2100f7073c365c46cf39d98dc2b81fb7f26
obada-naser <obadanaser135@gmail.com>
2021-11-01 08:51:41 +0200

    adding some changes

--- src/main/java/com/example/codefellowship/Controllers/AppUserControllers.java
+++ src/main/java/com/example/codefellowship/Controllers/AppUserControllers.java
@@ -6,9 +6,14 @@ import com.example.codefellowship.Repositories.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 
 @Controller
 public class AppUserControllers {
@@ -18,23 +23,38 @@ public class AppUserControllers {
     @Autowired
     AppUserRepository appUserRepository;
 
-    @GetMapping("/signUp")
+    @GetMapping("/signup")
     public String getSignUpPage(){
-        return "signUp";
+        return "signup";
     }
 
-    @PostMapping("/signUp")
-    public String signUpPage(@RequestParam String userName,
-                             @RequestParam String password,@RequestParam String firstName,@RequestParam String lastName,
-                             @RequestParam int dateOfBirth,@RequestParam String bio){
+    @GetMapping("/profile")
+    public String profilePage(Model model, Principal principal){
+        ApplicationUser applicationUser=appUserRepository.findByUserName(principal.getName());
+        model.addAttribute("userName",applicationUser);
+        return"profile";
+    }
+
+    @GetMapping("/users/{id}")
+    public String findingUser(@PathVariable int id,Model model){
+        model.addAttribute("userName",appUserRepository.findById(id));
+        return"profile";
+    }
+
+    @PostMapping("/signup")
+    public RedirectView signUpPage(@RequestParam String userName,
+                                   @RequestParam String password, @RequestParam String firstName, @RequestParam String lastName,
+                                   @RequestParam String dateOfBirth, @RequestParam String bio){
 
         ApplicationUser applicationUser=new ApplicationUser(userName,encoder.encode(password),firstName,lastName,dateOfBirth,bio);
 
         appUserRepository.save(applicationUser);
-        return "login";
+        return new RedirectView("/login");
 
 
     }
+
+
     @GetMapping("/login")
     public String getLoginPage(){
         return "login";
